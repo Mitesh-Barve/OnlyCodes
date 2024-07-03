d3bde29336325e3de03cd0a942a18d733b181c4c
Mitesh Barve <128367882+Mitesh-Barve@users.noreply.github.com>
2024-07-03 15:27:46 +0530

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -42,3 +42,53 @@ int main() {
    
     return 0;
 }
+*********************************************************************************************************XD****************************************************************************************
+    //USING RECURSION//
+    #include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+Node* createLinkedList(int arr[], int index, int size) {
+    // BASE CASE
+    if (index == size)
+        return NULL;
+
+    Node* temp = new Node(arr[index]);
+    temp->next = createLinkedList(arr, index + 1, size);
+
+    return temp;
+}
+
+int main() {
+
+    Node* Head = NULL;
+    int arr[] = {2, 4, 6, 8, 10};
+    Head = createLinkedList(arr, 0, 5);
+
+    Node* temp = Head;
+    while (temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    // Delete dynamically allocated memory to prevent memory leaks
+    temp = Head;
+    while (temp) {
+        Node* next = temp->next;
+        delete temp;
+        temp = next;
+    }
+
+    return 0;
+}
+
