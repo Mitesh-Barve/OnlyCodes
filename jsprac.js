38e4133c6f3bb32b8368ceeaebf7de5cfaa76883
Mitesh Barve <128367882+Mitesh-Barve@users.noreply.github.com>
2024-08-04 11:47:22 +0530

    Create jsprac.js

---
+++ jsprac.js
@@ -0,0 +1,27 @@
+class animal{
+    constructor(name , legcount , speaks){
+        this.name = name ;
+        this.legcount = legcount ;
+        this.speacks = speaks;
+    }
+    speacks(){
+        console.log("hi there"+ this.speaks);
+    }
+}
+let dog = new animal("dog",4,"bhow bhow");
+let cat = new animal("cat",4,"meow meow");
+cat.speaks();
+/********************************************************************/
+function square(a) {
+    return a * a;
+}
+
+function sumOfSomething(a, b, fn) {
+    const val1 = fn(a);
+    const val2 = fn(b);
+    return val1 + val2;
+}
+
+const a = 3;
+const b = 4;
+console.log(sumOfSomething(a, b, square)); // Output: 25
