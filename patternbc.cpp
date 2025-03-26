06a39735944cd1dbbf550fa19d9ae20fed23949f
Mitesh Barve <128367882+Mitesh-Barve@users.noreply.github.com>
2025-03-26 21:34:19 +0530

    Update patternbc.cpp

--- patternbc.cpp
+++ patternbc.cpp
@@ -20,3 +20,23 @@ b b b b b
 c c c c c 
 d d d d d 
 e e e e e 
+//*************************************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {   
+        for (char c = 'a'; c <= 'e'; c++) {  
+            cout << c << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+OP:-
+a b c d e 
+a b c d e 
+a b c d e 
+a b c d e 
+a b c d e 
+
