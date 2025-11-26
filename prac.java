1df28873c43fc7c47eada74d80562ddb421fe740
Mitesh Barve <128367882+Mitesh-Barve@users.noreply.github.com>
2025-11-26 12:54:50 +0530

    Create prac.java

---
+++ prac.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "java";
+        String rev = "";
+        int count = 0 ;
+              
+            for(int i=s.length()-1 ; i>= 0 ; i--){
+                rev = rev + s.charAt(i);
+            }
+
+        for(int i = 0 ; i <= s.length()-1 ; i++){
+             char ch = s.charAt(i) ;
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                count++;
+        }
+        
+        System.out.println(rev);
+        System.out.println(count);
+    }
