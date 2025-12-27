c0aed04d8211d218dae98d4982435db5b2f2e901
Mitesh Barve <128367882+Mitesh-Barve@users.noreply.github.com>
2025-12-27 19:30:03 +0530

    Update prac.java

--- prac.java
+++ prac.java
@@ -17,3 +17,24 @@ public class Main {
         System.out.println(rev);
         System.out.println(count);
     }
+/**********************************************************************************************************/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        for (int i = 0; i < n; i++
